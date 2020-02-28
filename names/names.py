8fb85e1ff60c8610c1a7184a7a75d1d5c055e0b4
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-02-28 11:04:36 +0100

    Implement reverse list

--- names/names.py
+++ names/names.py
@@ -13,10 +13,15 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+# combo = name_1 + name_2
+
+for i in range(len(names_1)-1):
+    if names_1[i] in names_2:
+        duplicates.append(names_1[i])
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
